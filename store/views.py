0076ebc3a46549d1b361443e9118d7416566900b
Andreea Ivascu <112897937+andreeaivi@users.noreply.github.com>
2023-05-08 22:09:45 +0300

    Ver 4

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from.models import *
 from django.views.generic import TemplateView
 
@@ -8,9 +10,32 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,'get_cart_items':0 }
+
+
+    context = {'items' :items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,'get_cart_items':0 }
+
+
+    context = {'items' :items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
